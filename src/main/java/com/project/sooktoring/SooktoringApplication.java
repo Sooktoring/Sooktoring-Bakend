d4461fd79a669a7ae9772450dca3ce329d567593
jkde7721 <jkde7721@naver.com>
2022-04-16 13:14:02 +0900

    init project

---
+++ src/main/java/com/project/sooktoring/SooktoringApplication.java
@@ -0,0 +1,13 @@
+package com.project.sooktoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SooktoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SooktoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sooktoring/SooktoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sooktoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SooktoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
