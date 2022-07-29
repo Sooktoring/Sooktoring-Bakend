0f6e3a37242a40fa8d392f5051052746353b78cb
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-07-29 12:47:56 +0900

    이미지 업로드 설정

--- src/main/java/com/project/sooktoring/SooktoringApplication.java
+++ src/main/java/com/project/sooktoring/SooktoringApplication.java
@@ -8,6 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class SooktoringApplication {
 
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SooktoringApplication.class, args);
 	}
