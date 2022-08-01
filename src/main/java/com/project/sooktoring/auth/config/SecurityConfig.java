7e6fba80b6cec16df955eb1d4249d97fe80b4766
jkde7721 <jkde7721@naver.com>
2022-08-01 12:25:00 +0900

    SecurityConfig 수정
    WebSecurityConfigurerAdapter 삭제

--- src/main/java/com/project/sooktoring/auth/config/SecurityConfig.java
+++ src/main/java/com/project/sooktoring/auth/config/SecurityConfig.java
@@ -2,43 +2,45 @@ package com.project.sooktoring.auth.config;
 
 import com.project.sooktoring.auth.exception.exhandler.AuthExceptionHandlerFilter;
 import com.project.sooktoring.auth.jwt.JwtAuthenticationFilter;
-import com.project.sooktoring.auth.jwt.AuthTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
 
-    private final AuthTokenProvider tokenProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthExceptionHandlerFilter authExceptionHandlerFilter;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().formLogin().disable()
+                .httpBasic().disable()
+                .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .antMatchers("/v3/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**", "/auth/**", "/", "/test").permitAll()
                 .antMatchers("/img/*").permitAll() //이미지 업로드 테스트
                 .antMatchers("/mentoring/to").hasRole("MENTOR")
 //                .antMatchers("/mentee").hasRole("MENTEE")
                 .anyRequest().authenticated().and()
-                .headers()
-                .frameOptions()
-                .sameOrigin().and()
-                .cors().and()
-                .csrf().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(authExceptionHandlerFilter, JwtAuthenticationFilter.class);
+
+        return http.build();
     }
 }
