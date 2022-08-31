443bd0d4e0b6c24c690beaba5a965c7cdb4db503
jkde7721 <jkde7721@naver.com>
2022-08-31 13:43:40 +0900

    푸시 알림 테스트 컨트롤러 메소드 생성

--- src/main/java/com/project/sooktoring/push/controller/FcmController.java
+++ src/main/java/com/project/sooktoring/push/controller/FcmController.java
@@ -1,7 +1,5 @@
 package com.project.sooktoring.push.controller;
 
-import com.project.sooktoring.auth.domain.User;
-import com.project.sooktoring.common.utils.UserUtil;
 import com.project.sooktoring.push.service.FcmService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
@@ -21,21 +19,14 @@ import static org.springframework.http.HttpStatus.*;
 @RestController
 public class FcmController {
 
-    private final UserUtil userUtil;
     private final FcmService fcmService;
 
-    @Operation(summary = "현재 유저의 디바이스로 푸시 알림 전송 (삭제 예정)", responses = {
-            @ApiResponse(responseCode = "200", description = "정상적으로 푸시 알림 전송된 경우"),
-            @ApiResponse(responseCode = "401", description = "유효하지 않은 엑세스 토큰"),
-            @ApiResponse(responseCode = "404", description = "존재하지 않는 유저 or FCM 토큰")
-    })
-    @PostMapping
-    public ResponseEntity<Void> pushNotification() throws IOException {
-        User user = userUtil.getCurrentUser();
-        fcmService.sendMessageTo(user.getFcmToken(), "hi", "hello world! wow~~~~~~~~~~~~~~~!!");
+    @Operation(summary = "푸시 알림 전송 테스트 (삭제 예정)")
+    @PostMapping("/test")
+    public ResponseEntity<Void> pushNotification(@Parameter(description = "파이어베이스 등록 토큰") @RequestParam String targetToken) throws IOException {
+        fcmService.sendMessageTo(targetToken, "hi", "hello world! wow~~~~~~~~~~~~~~~!!");
         return ResponseEntity.status(OK).build();
     }
-
 
     @Operation(summary = "현재 유저의 파이어베이스 등록 토큰 업데이트", responses = {
             @ApiResponse(responseCode = "200", description = "정상적으로 파이어베이스 등록 토큰 업데이트된 경우"),
